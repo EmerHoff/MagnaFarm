ffe0befe6aafce7bebc76ca025e053fdade08afa
EmerHoff <emerson_h2010@hotmail.com>
2020-11-21 15:21:17 -0300

    command

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import './database/connect';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const port: number = parseInt(`${process.env.PORT}`) || 3000;
-  await app.listen(port, () => console.log('Server started in http://localhost:3333'));
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => console.log('Server started in http://localhost' + port));
 }
 bootstrap();
