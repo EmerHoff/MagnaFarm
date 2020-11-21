19a128cb41d0e8691ead19b1324deb8f0dd09c97
EmerHoff <emerson_h2010@hotmail.com>
2020-11-21 12:40:19 -0300

    heroku config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import './database/connect';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3333, () => console.log('Server started in http://localhost:3333'));
+  await app.listen(process.env.PORT || 3333, () => console.log('Server started in http://localhost:3333'));
 }
 bootstrap();
